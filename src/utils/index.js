c4d86d2f7a6583f142bc556c43abe58229aed895
habib almaula <habibalmaula@gmail.com>
2023-03-24 09:50:34 +0700

    fixing : songs & albums

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase */
+const mapAlbumToModel = (data) => {
+  console.log(data);
+  const songs = data.reduce((acc, row) => {
+    if (row.song_id != null && row.title != null && row.performer != null) {
+      acc.push({
+        id: row.song_id,
+        title: row.title,
+        performer: row.performer,
+      });
+    }
+    return acc;
+  }, []);
+  return {
+    id: data[0].id,
+    name: data[0].name,
+    year: data[0].year,
+    songs: songs ?? '',
+  };
+};
+
+module.exports = {mapAlbumToModel};
